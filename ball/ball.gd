e7fa5146d7ba67bc315c4c616a54a928a3e6a399
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-08 17:56:04 +0100

    Ball bounces off of bricks
    :D

--- ball/ball.gd
+++ ball/ball.gd
@@ -4,7 +4,7 @@ class_name Ball
 
 const SPEED = 150
 
-var direction = Vector2(1, -1)
+@export var direction = Vector2(0, 1)
 
 
 func _ready():
@@ -20,3 +20,18 @@ func _physics_process(_delta):
 		direction.y = direction.y * -1
 	velocity = direction * SPEED 
 	move_and_slide()
+
+
+func bounce_off():
+	if direction.angle() <= PI/4 and direction.angle() >= -PI/4:
+		# Ball is moving right
+		direction.x = -abs(direction.x)
+	elif direction.angle() <= -PI/4 and direction.angle() > -(PI/4)*3:
+		# Ball is moving up
+		direction.y = abs(direction.y)
+	elif direction.angle() >= PI/4 and direction.angle() < (PI/4)*3:
+		# Ball is moving down
+		direction.y = -abs(direction.y)
+	else:
+		# Ball is moving left
+		direction.x = abs(direction.x)
---
+++ brick/brick.gd
@@ -0,0 +1,10 @@
+extends Area2D
+class_name Brick
+
+
+@onready var sprite := $RedBrick
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Balls"):
+		body.bounce_off()
