6436cc22bba6d6268f9ccc681326c0421a03a8cb
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-23 13:38:05 +0100

    Delete unused parts of code

--- ball/ball.gd
+++ ball/ball.gd
@@ -21,14 +21,22 @@ func _ready():
 
 
 func _physics_process(_delta):
-	if ray_down.get_collider():
-		print("down")
-	if ray_left.get_collider():
-		print("left")
-	if ray_right.get_collider():
-		print("right")
-	if ray_up.get_collider():
-		print("up")
+	var _down_col = ray_down.get_collider()
+	if _down_col:
+		print(_down_col)
+		_down_col.lower_endurance()
+	var _left_col = ray_left.get_collider()
+	if _left_col:
+		print(_left_col)
+		_left_col.lower_endurance()
+	var _right_col = ray_right.get_collider()
+	if _right_col:
+		print(_right_col)
+		_right_col.lower_endurance()
+	var _up_col = ray_up.get_collider()
+	if _up_col:
+		print(_up_col)
+		_up_col.lower_endurance()
 	if attached:
 		position = get_parent().deflector.ball_holder.global_position
 		if Input.is_action_just_pressed("ui_accept"):
@@ -48,86 +56,3 @@ func remove_or_return(_reattach: bool = false):
 	if _reattach:
 		attached = true
 	return ID
-
-#
-#func bounce_off(_brick_pos: Vector2, _brick_size: Vector2):
-	## Calculate Ball's size
-	#var _brick_edge_top: int = (_brick_pos.y - (_brick_size.y / 2)) as int
-	#var _brick_edge_bottom: int =( _brick_pos.y + (_brick_size.y / 2)) as int
-	#var _brick_edge_left: int = (_brick_pos.x - (_brick_size.x / 2)) as int
-	#var _brick_edge_right: int = (_brick_pos.x + (_brick_size.x / 2)) as int
-	#var _ball_size := Vector2(
-		#sprite.get_rect().size.x * sprite.scale.x,
-		#sprite.get_rect().size.y * sprite.scale.y
-	#)
-	#var _ball_edge_top: int = (global_position.y - (_ball_size.y / 2)) as int
-	#var _ball_edge_bottom: int = (global_position.y + (_ball_size.y / 2)) as int
-	#var _ball_edge_left: int = (global_position.x - (_ball_size.x / 2)) as int
-	#var _ball_edge_right: int = (global_position.x + (_ball_size.x / 2)) as int
-	#print(direction.angle())
-	#print(PI/4)
-	#print((PI/4)*3)
-	#if direction.angle() <= PI/4 and direction.angle() >= -PI/4:
-		#print("Reflect after right")
-		## Ball is moving right
-		## Calculating edges
-		##var _brick_edge_top: int = (_brick_pos.y - (_brick_size.y / 2)) as int
-		##var _brick_edge_bottom: int =( _brick_pos.y + (_brick_size.y / 2)) as int
-		##var _ball_edge_top: int = (global_position.y - (_ball_size.y / 2)) as int
-		##var _ball_edge_bottom: int = (global_position.y + (_ball_size.y / 2)) as int
-		## Ball is between brick's edges
-		#var _condition_1: bool = (_ball_edge_top >= _brick_edge_top) and (_ball_edge_bottom <= _brick_edge_bottom)
-		## Ball is still below brick's upper edge
-		#var _condition_2: bool = (_ball_edge_bottom >= _brick_edge_top)
-		## Ball is still above brick's lower edge
-		#var _condition_3: bool = (_ball_edge_top <= _brick_edge_bottom)
-		#if _condition_1 or _condition_2 or _condition_3:
-			#direction.x = -abs(direction.x)
-	#elif direction.angle() <= -PI/4 and direction.angle() > -(PI/4)*3:
-		#print("Reflect after up")
-		## Ball is moving up
-		## Calculating edges
-		##var _brick_edge_left: int = (_brick_pos.x - (_brick_size.x / 2)) as int
-		##var _brick_edge_right: int = (_brick_pos.x + (_brick_size.x / 2)) as int
-		##var _ball_edge_left: int = (global_position.x - (_ball_size.x / 2)) as int
-		##var _ball_edge_right: int = (global_position.x + (_ball_size.x / 2)) as int
-		## Ball is between brick's edges
-		#var _condition_1: bool = (_ball_edge_left >= _brick_edge_left) and (_ball_edge_right <= _brick_edge_right)
-		## Ball touches brick's left edge
-		#var _condition_2: bool = (_ball_edge_right >= _brick_edge_left)
-		## Ball touches brick's right edge 
-		#var _condition_3: bool = (_ball_edge_left <= _brick_edge_right)
-		#if _condition_1 or _condition_2 or _condition_3:
-			#direction.y = abs(direction.y)
-	#elif direction.angle() >= PI/4 and direction.angle() < (PI/4)*3:
-		#print("Reflect after down")
-		## Ball is moving down
-		## Calculating edges
-		##var _brick_edge_left: int = (_brick_pos.x - (_brick_size.x / 2)) as int
-		##var _brick_edge_right: int = (_brick_pos.x + (_brick_size.x / 2)) as int
-		##var _ball_edge_left: int = (global_position.x - (_ball_size.x / 2)) as int
-		##var _ball_edge_right: int = (global_position.x + (_ball_size.x / 2)) as int
-		## Ball is between brick's edges
-		#var _condition_1: bool = (_ball_edge_left >= _brick_edge_left) and (_ball_edge_right <= _brick_edge_right)
-		## Ball touches brick's left edge
-		#var _condition_2: bool = (_ball_edge_right >= _brick_edge_left)
-		## Ball touches brick's right edge 
-		#var _condition_3: bool = (_ball_edge_left <= _brick_edge_right)
-		#if _condition_1 or _condition_2 or _condition_3:
-			#direction.y = -abs(direction.y)
-	#else:
-		#print("Reflect after left")
-		## Ball is moving left
-		## Calculating edges
-		##var _brick_edge_top: int = (_brick_pos.y - (_brick_size.y / 2)) as int
-		##var _brick_edge_bottom: int =( _brick_pos.y + (_brick_size.y / 2)) as int
-		##var _ball_edge_top: int = (global_position.y - (_ball_size.y / 2)) as int
-		##var _ball_edge_bottom: int = (global_position.y + (_ball_size.y / 2)) as int
-		## Ball is between brick's edges
-		#var _condition_1: bool = (_ball_edge_top >= _brick_edge_top) and (_ball_edge_bottom <= _brick_edge_bottom)
-		## Ball is still below brick's upper edge
-		#var _condition_2: bool = (_ball_edge_bottom >= _brick_edge_top)
-		## Ball is still above brick's lower edge
-		#var _condition_3: bool = (_ball_edge_top <= _brick_edge_bottom)
-		#if _condition_1 or _condition_2 or _condition_3:
-			#direction.x = abs(direction.x)
--- brick/brick.gd
+++ brick/brick.gd
@@ -2,16 +2,15 @@ extends CharacterBody2D
 class_name Brick
 
 
+@export var endurace: int = 1
+
 @onready var sprite := $RedBrick
 
 
-#func _on_body_entered(body):
-	#if body.is_in_group("Balls"):
-		#body.bounce_off(
-			#global_position,
-			#Vector2(
-				#sprite.get_rect().size.x * sprite.scale.x, 
-				#sprite.get_rect().size.y * sprite.scale.y
-				#)
-			#)
-		# initiate destruction of the brick or at leas lower it's endurance
+func lower_endurance():
+	endurace -= 1
+	if endurace < 1:
+		destory_self()
+
+func destory_self():
+	pass
