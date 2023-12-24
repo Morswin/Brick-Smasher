6b3f16bbe95dd8fb5c0db07b0a264c53a04203e1
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-24 14:04:26 +0100

    Fixes #4

---
+++ main menu/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+var game_loop := preload("res://game_loop.tscn")
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_packed(game_loop)
+
+
+func _on_options_pressed():
+	# TODO implement options menu
+	print("Not implemented yet")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
