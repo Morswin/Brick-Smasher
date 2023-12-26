fd3807d09bad5fa2d9cfd924d95f91e31ac7a9dd
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-26 14:06:03 +0100

    Brick may be generated

--- brick/brick_row.gd
+++ brick/brick_row.gd
@@ -2,14 +2,24 @@ extends Node2D
 class_name BrickRow
 
 
-@onready var brick_scene := load("res://brick/brick.gd")
+const brick_scene = preload("res://brick/brick.tscn")
 
 var center = 576  # This is where brick row's x should be set up to
 
-@export var pos_first = 64
-@export var pos_change = 32
-@export var pos_last = 1088
+@export var pos_first := 64.0  # pos 0
+@export var pos_change := 32.0
+@export var pos_last := 1088.0  # pos 32
 
 
 func add_brick(_x: int) -> void:
-	pass
+	# _x Value between 0 and 32
+	var _brick_pos: float
+	if _x < 0:
+		_brick_pos = pos_first
+	elif _x > 32:
+		_brick_pos = pos_last
+	else:
+		_brick_pos = (_x * pos_change) + pos_first
+	var _new_brick = brick_scene.instantiate()
+	_new_brick.position.x = _brick_pos
+	add_child(_new_brick)
--- brick/bricks.gd
+++ brick/bricks.gd
@@ -2,12 +2,19 @@ extends Node2D
 class_name BrickRowManager
 
 
-@onready var brick_row_scene = load("res://brick/brick_row.tscn")
+const brick_row_scene = preload("res://brick/brick_row.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("test")
+	var _new_birck_row = brick_row_scene.instantiate()
+	_new_birck_row.position.y = 48
+	_new_birck_row.add_brick(0)
+	_new_birck_row.add_brick(1)
+	_new_birck_row.add_brick(16)
+	_new_birck_row.add_brick(32)
+	add_child(_new_birck_row)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
