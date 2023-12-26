1ade4506a5db975cfe46a0ad271d9838609fd7d1
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-26 19:24:15 +0100

    Row manager refactor

--- brick/bricks.gd
+++ brick/bricks.gd
@@ -4,19 +4,31 @@ class_name BrickRowManager
 
 const brick_row_scene = preload("res://brick/brick_row.tscn")
 
+var rng = RandomNumberGenerator.new()
+var rows = {}
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	print("test")
-	var _new_birck_row = brick_row_scene.instantiate()
-	_new_birck_row.position.y = 48
-	_new_birck_row.add_brick(0)
-	_new_birck_row.add_brick(2)
-	_new_birck_row.add_brick(16)
-	_new_birck_row.add_brick(32)
-	add_child(_new_birck_row)
+	#var _new_birck_row = brick_row_scene.instantiate()
+	#_new_birck_row.position.y = 48
+	#_new_birck_row.add_brick(0)
+	#_new_birck_row.add_brick(2)
+	#_new_birck_row.add_brick(16)
+	#_new_birck_row.add_brick(32)
+	#add_child(_new_birck_row)
+	add_row()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func add_row():
+	rng.randomize()
+	var _new_brick_row = brick_row_scene.instantiate()
+	_new_brick_row.position.y = 48  # It will be hidden at the start under the upper edge
+	var _space_needed: bool = false  # if true, loop should skip over creating such a brick
+	for _brick_x_pos in range(0, 33):
+		if _space_needed:
+			_space_needed = false
+		elif rng.randi() % 2 == 0:
+			_new_brick_row.add_brick(_brick_x_pos)
+			_space_needed = true
+	add_child(_new_brick_row)
