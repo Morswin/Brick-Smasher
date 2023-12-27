a1730e4934634c099377cfd894a0c4532c29073b
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-27 14:34:07 +0100

    Fixes #15

--- options menu/options.gd
+++ options menu/options.gd
@@ -1,8 +1,16 @@
 extends Control
 
 
-var main_menu = preload("res://main menu/main_menu.tscn")
+@onready var full_screen_cb := $MarginContainer/VBoxContainer/HBoxContainer/CheckBox
+
+var main_menu := preload("res://main menu/main_menu.tscn")
 
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_packed(main_menu)
+
+func _on_check_box_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
