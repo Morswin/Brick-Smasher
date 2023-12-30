25a9a241b8dee95dd01a77f3ae8ab621dde4ac77
Morswin <41349347+Morswin@users.noreply.github.com>
2023-12-30 18:05:02 +0100

    Remove unused _id code lines

--- ball/ball_manager.gd
+++ ball/ball_manager.gd
@@ -14,11 +14,9 @@ func _ready():
 	var _new_ball = BALL_SCENE.instantiate()
 	_new_ball.position = Vector2(64, 64)
 	_new_ball.attached = true
-	var _id = len(ball_list)
-	_new_ball.ID = _id
 	add_child(_new_ball)
 	ball_list.append(_new_ball)
 
 func _on_ball_catcher_body_entered(body):
 	if body.is_in_group("Balls"):
-		var _id = body.remove_or_return(len(ball_list) <= 1)
+		body.remove_or_return(len(ball_list) <= 1)
